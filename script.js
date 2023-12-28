5a91f6b73281b7e4a5611ee0b607e577bb378c73
Taras Ishchuk <ishchuktaras@gmail.com>
2023-12-28 12:09:45 +0100

    ovládáme string a number

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // console.log("Učíme se Java Script");
 
+// String
 let firstName = "Taras";
 let secondName = "Ishchuk";
 let city = "Jihlava";
 let job = "vývojář webových stránek";
 
-console.log('Ahoj, jmenuji se ' + firstName + " " + secondName + " a pracuji jako " + job);
+// Number
+let age = 40;
+let feature = 5;
+let featureAge = age + feature;
+console.log(
+  "Ahoj, jmenuji se " +
+    firstName +
+    " " +
+    secondName +
+    " a pracuji jako " +
+    job +
+    ". " +
+    "Je mi " +
+    age +
+    " let."
+);
+
+console.log("Za " + feature + " let mi bude " + featureAge + '.');
