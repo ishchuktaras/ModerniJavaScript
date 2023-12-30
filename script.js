79bfda711d527d30e58f18923f548a193f62f66f
Taras Ishchuk <ishchuktaras@gmail.com>
2023-12-30 18:58:36 +0100

    Zlepšení codu

--- script.js
+++ script.js
@@ -1,51 +1,47 @@
 // console.log("Učíme se Java Script");
 
 // String
-let firstName = "Taras";
-secondName = "Ishchuk";
-city = "Jihlava";
-lob = "vývojář webových stránek";
+let firstName = "Taras",
+  secondName = "Ishchuk",
+  city = "Jihlava",
+  lob = "vývojář webových stránek",
+  // Number
+  age = 21,
+  feature = 5,
+  featureAge = age + feature,
+  student1 = 65,
+  student2 = 77,
+  student1Name = "Vavzhyniak",
+  student2Name = "Ishchuk",
+  maxPoints = 150,
+  success1 = (student1 / maxPoints) * 100,
+  success2 = (student2 / maxPoints) * 100,
+  child = age < 18,
+  adult = age >= 18,
+  pensioner = age >= 65;
 
-// Number
-age = 15;
-feature = 5;
-featureAge = age + feature;
+  // console.log(
+  //   "Ahoj, jmenuji se " +
+  //     firstName +
+  //     " " +
+  //     secondName +
+  //     " a pracuji jako " +
+  //     job +
+  //     ". " +
+  //     "Je mi " +
+  //     age +
+  //     " let."
+  // );
 
-student1 = 65;
-student2 = 77;
+  // console.log("Za " + feature + " let mi bude " + featureAge + ".");
 
-student1Name = "Vavzhyniak";
-student2Name = "Ishchuk";
+  // String, numbers a matematické operace
 
-maxPoints = 150;
+  // console.log(student1Name + " dosáhl(a) " + success1 + "% uspěšnosti");
+  // console.log(student2Name + " dosáhl(a) " + success2 + "% uspěšnosti");
 
-success1 = (student1 / maxPoints) * 100;
-success2 = (student2 / maxPoints) * 100;
+  // Boolean
 
-child = age <= 18;
-adult = age >= 18 <= 65;
-pensioner = age >= 65;
-
-// console.log(
-//   "Ahoj, jmenuji se " +
-//     firstName +
-//     " " +
-//     secondName +
-//     " a pracuji jako " +
-//     job +
-//     ". " +
-//     "Je mi " +
-//     age +
-//     " let."
-// );
-
-// console.log("Za " + feature + " let mi bude " + featureAge + ".");
-
-// ovládáme string, numbers a matematické operace
-
-// console.log(student1Name + " dosáhl(a) " + success1 + "% uspěšnosti");
-// console.log(student2Name + " dosáhl(a) " + success2 + "% uspěšnosti");
-
-console.log('Dítě: ' + child);
-console.log('Dospělý: ' + adult);
-console.log('Dúchodce: ' + pensioner);
+console.log("Dítě: " + child);
+console.log("Dospělý: " + adult);
+console.log("Dúchodce: " + pensioner);
